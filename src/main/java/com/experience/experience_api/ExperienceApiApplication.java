138df984980cb72d5381c0f21c93883cde5d67ea
Rishikesh Sahu <rsahu@enphaseenergy.com>
2020-10-02 10:06:45 +0530

    Initial Setup

---
+++ src/main/java/com/experience/experience_api/ExperienceApiApplication.java
@@ -0,0 +1,13 @@
+package com.experience.experience_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperienceApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/experience/experience_api/repository/ExperienceRepository.java
@@ -0,0 +1,5 @@
+package com.experience.experience_api.repository;
+
+public interface ExperienceRepository {
+
+}
